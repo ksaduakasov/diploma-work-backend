3eca77df21dc92151326e12c546e911ce41315a9
Beibarys Zholmyrza <beibaryszholmyrza@gmail.com>
2022-05-10 05:42:45 +0000

    Added: authentification

---
+++ diploma-backend/src/main/java/com/example/diplomawork/service/AuthService.java
@@ -0,0 +1,110 @@
+package com.example.diplomawork.service;
+
+import com.example.diplomawork.exception.SpringAppException;
+import com.example.diplomawork.model.User;
+import com.example.diplomawork.model.VerificationToken;
+import com.example.diplomawork.repository.UserRepository;
+import com.example.diplomawork.repository.VerificationTokenRepository;
+import com.example.diplomawork.security.JwtProvider;
+import com.example.models.AuthenticationResponse;
+import com.example.models.LoginRequest;
+import com.example.models.RefreshTokenRequest;
+import com.example.models.RegisterRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
+
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+
+        userRepository.save(user);
+    }
+
+
+    @Transactional(readOnly = true)
+    public User getCurrentUser() {
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.
+                getContext().getAuthentication().getPrincipal();
+        return userRepository.findByUsername(principal.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("User name not found - " + principal.getUsername()));
+    }
+
+    private void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringAppException("User not found with name - " + username));
+        userRepository.save(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        fetchUserAndEnable(verificationToken.orElseThrow(() -> new SpringAppException("Invalid Token")));
+    }
+
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        String token = jwtProvider.generateToken(authenticate);
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        authenticationResponse.setAuthenticationToken(token);
+        authenticationResponse.setRefreshToken(refreshTokenService.generateRefreshToken().getToken());
+        authenticationResponse.setExpiresAt(OffsetDateTime.now().plusSeconds((jwtProvider.getJwtExpirationInMillis())));
+        authenticationResponse.setUsername(loginRequest.getUsername());
+        return authenticationResponse;
+    }
+
+    public AuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.validateRefreshToken(refreshTokenRequest.getToken());
+        String token = jwtProvider.generateTokenWithUserName(refreshTokenRequest.getUsername());
+
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        authenticationResponse.setAuthenticationToken(token);
+        authenticationResponse.setRefreshToken(refreshTokenRequest.getToken());
+        authenticationResponse.setExpiresAt(OffsetDateTime.now().plusSeconds((jwtProvider.getJwtExpirationInMillis())));
+        authenticationResponse.setUsername(refreshTokenRequest.getUsername());
+        return authenticationResponse;
+    }
+
+    public boolean isLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return !(authentication instanceof AnonymousAuthenticationToken) && authentication.isAuthenticated();
+    }
+
+
+
+}
