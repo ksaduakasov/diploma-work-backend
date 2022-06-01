d5d792001088b213c53fba32e4a756e41c8c9d86
ksaduakasov1 <Kalbek2002>
2022-06-01 10:09:19 +0600

    Reviewers added

--- diploma-backend/src/main/java/com/example/diplomawork/repository/ReviewerRepository.java
+++ diploma-backend/src/main/java/com/example/diplomawork/repository/ReviewerRepository.java
@@ -1,8 +1,9 @@
 package com.example.diplomawork.repository;
 
 import com.example.diplomawork.model.Reviewer;
+import com.example.diplomawork.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewerRepository extends JpaRepository<Reviewer, Long> {
-
+    Reviewer findByName(String name);
 }
